1630d8992b9c95b4eb00977d9dd5c3bc1f6dd2e5
SpaghettiCoder101 <spaghetticoder101@gmail.com>
2023-05-15 08:07:55 +0200

    Added base style config

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { styled } from "../stitches.config.ts";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <Page>
+            <p>Test</p>
+        </Page>
+    );
 }
 
-export default App
+
+const Page = styled("div", {
+    display: "flex",
+    flexGrow: 1,
+    flexDirection: "row",
+    backgroundColor: "$background",
+    maxWidth:"1414px",
+    maxHeight: "2000px",
+    margin: "0 auto",
+});
---
+++ stitches.config.ts
@@ -0,0 +1,11 @@
+import { createStitches } from "@stitches/react";
+
+export const { styled, css, globalCss, keyframes, getCssText, theme, createTheme, config } = createStitches({
+    theme: {
+        colors: {
+            accent: "#FF914D",
+            dark:"#494E5F",
+            background: "#FEFEFA",
+        },
+    },
+});
