2eda29020f9cec32f15af53a72ab2fcbb16e35ff
SpaghettiCoder101 <spaghetticoder101@gmail.com>
2023-05-17 18:21:17 +0200

    Added personal info

--- src/components/InfoDetails.tsx
+++ src/components/InfoDetails.tsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import { styled } from "../../stitches.config";
+import { useStrings } from "../localization/hooks/useStrings";
 
 type Props = {
     open: boolean;
@@ -11,6 +12,7 @@ const variants = {
 };
 
 export default function InfoDetails({ open }: Props) {
+    const strings = useStrings();
     return (
         <motion.div
             animate={open ? "open" : "closed"}
@@ -18,10 +20,20 @@ export default function InfoDetails({ open }: Props) {
             transition={{ duration: 0.5 }}
         >
             <Wrapper>
-                <Title>InfoDetails</Title>
-                <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe quasi, ullam quia, pariatur reiciendis veritatis, at animi earum cumque sint doloribus. Facere dolores quos illum assumenda nesciunt id iusto a!
-                </p>
+                <Title>{strings.common$about}</Title>
+                <Text>
+                    {strings.about$presentation}
+                </Text>
+                <Title>{strings.common$contacts}</Title>
+                <Text>
+                    {strings.contacts$phone}<br /><Link href={"tel:" + strings.data$phone}> {strings.data$phone}</Link>
+                </Text>
+                <Text>
+                    {strings.contacts$mail}<br /><Link href={"mailto:" + strings.data$mail}> {strings.data$mail}</Link>
+                </Text>
+                <Text>
+                    {strings.contacts$address}<br />{strings.data$address}
+                </Text>
             </ Wrapper>
         </motion.div>
     );
@@ -39,4 +51,19 @@ const Title = styled("h2", {
     color: "$accent",
     marginBottom: "0.5rem",
     textTransform: "uppercase",
+});
+
+const Text = styled("p", {
+    fontSize: "1.2rem",
+    color: "$background",
+    marginBottom: "1rem",
+    marginTop: "0",
+});
+
+const Link = styled("a", {
+    color: "$background",
+    textDecoration: "underline",
+    "&:hover": {
+        color: "$accent",
+    },
 });
