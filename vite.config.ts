b93d312abf2ec5425aaeb1486fe0b6fd7f89454d
SpaghettiCoder101 <spaghetticoder101@gmail.com>
2023-05-24 09:24:57 +0200

    added gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import eslint from "vite-plugin-eslint"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    eslint({fix: true}),
-    react()
-],
-})
+    base: "/web-resume",
+    plugins: [
+        eslint({ fix: true }),
+        react(),
+    ],
+});
